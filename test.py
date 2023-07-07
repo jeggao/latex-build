e9c07930d50ef51f4d84c0f78347eb18911ccfef
Jerry Gao <email__address@163.com>
2023-07-07 09:04:14 -0400

    Add tests

---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+from time import perf_counter
+
+from project_name import build as target
+
+
+class TestBuild(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        os.chdir("./project_name")
+
+    def test_default(self):
+        """
+        Test normal build.
+        """
+        target.build(live=False, draft=False, verbose=False)
+        target.run(["rm", "-r", "tmp/"])
+
+    def test_verbose(self):
+        """
+        Test normal build.
+        """
+        target.build(live=False, draft=False, verbose=True)
+        target.run(["rm", "-r", "tmp/"])
+
+    def test_draft(self):
+        """
+        Test normal build.
+        """
+        target.build(live=False, draft=True, verbose=False)
+        target.run(["rm", "-r", "tmp/"])
+
+
+if __name__ == "__main__":
+    unittest.main()
