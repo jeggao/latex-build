4580fb2f759a4b12549df98bf928800fe0ef78e7
Jerry Gao <jerrygao.email@gmail.com>
2023-08-08 09:13:58 -0400

    Remove unportable tests

--- test.py
+++ test.py
@@ -35,56 +35,6 @@ class TestParse(unittest.TestCase):
         )
 
 
-class TestBuild(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        chdir("./project_name")
-
-    def test_default(self):
-        """
-        Test normal build.
-        """
-        target.build(
-            build_config={
-                "latex-build": {"version": "v2.0"},
-                "latexmk": {
-                    "LATEX": "pdflatex",
-                    "TEX_NAME": "main.tex",
-                    "AUX_DIR": "./tmp",
-                    "OUT_DIR": "./",
-                    "OUT_NAME": "main",
-                },
-            },
-            live=False,
-            draft=False,
-            spell=False,
-            verbose=True,
-        )
-        shutil.rmtree("tmp/", ignore_errors=True)
-
-    def test_draft(self):
-        """
-        Test normal build.
-        """
-        target.build(
-            build_config={
-                "latex-build": {"version": "v2.0"},
-                "latexmk": {
-                    "LATEX": "pdflatex",
-                    "TEX_NAME": "main.tex",
-                    "AUX_DIR": "./tmp",
-                    "OUT_DIR": "./",
-                    "OUT_NAME": "main",
-                },
-            },
-            live=False,
-            draft=True,
-            spell=False,
-            verbose=True,
-        )
-        shutil.rmtree("tmp/", ignore_errors=True)
-
-
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
     unittest.main()
